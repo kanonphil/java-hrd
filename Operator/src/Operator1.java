37e1ad083c30e070fefc4d33445c15ee86e2420b
kanonphil <xoghkdbwjd@gmail.com>
2025-12-16 15:37:02 +0900

    Operator Example

---
+++ Operator/src/Operator1.java
@@ -0,0 +1,11 @@
+public class Operator1 {
+  public static void main(String[] args) {
+    //산술 연산자 (+, -, *, /, %)
+    // '%' : mod 연산자, 나눗셈의 나머지를 구하는 연산자
+    int result1 = 10 % 4;
+    System.out.println(result1);
+
+    //비교 연산자
+    // >, <, >=, <=,
+  }
+}
