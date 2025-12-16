ed23176365dd762f1cf28873c11e790f3f2b8004
kanonphil <xoghkdbwjd@gmail.com>
2025-12-16 15:11:26 +0900

    DataType example

---
+++ Variable/src/DataType1.java
@@ -0,0 +1,39 @@
+public class DataType1 {
+  public static void main(String[] args) {
+    int num1 = 10;
+    byte num2 = 100;
+    short num3 = 100;
+    long num4 = 100;
+
+    double num6 = 10.5;
+    float num5 = 10.5f;
+
+    boolean num7 = true;
+    System.out.println(num7);
+
+    //기본 자료형(8개/문자열x)
+    //문자는 무조건 홀따옴표 ''
+    char c = 'a';
+    System.out.println(c);
+
+    //참조 자료형(무한대/앞글자는 대문자)
+    //문자열은 쌍따옴표 ""
+    String str = "홍길동";
+    System.out.println(str);
+
+//    boolean bool = false;
+//    System.out.println("bool = " + bool);
+//
+//    byte num1 = 10;
+//    System.out.println("num1 = " + num1);
+//
+//    int num2 = 20;
+//    System.out.println("num2 = " + num2);
+//
+//    double num3 = 10.5;
+//    System.out.println("double = " + num3);
+//
+//    char word = '가';
+//    System.out.println("word = " + word);
+  }
+}
