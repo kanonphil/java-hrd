ee55a79a34146e066b32997d9918456491f0e96b
kanonphil <xoghkdbwjd@gmail.com>
2025-12-16 16:36:49 +0900

    Operator Example

--- Operator/src/Operator2.java
+++ Operator/src/Operator2.java
@@ -15,5 +15,24 @@ public class Operator2 {
     int num4 = num3++;
     System.out.println(num4); //6
     System.out.println(num3); //7
+
+    //복합대입연산자
+    int a = 3;
+    int b = 7;
+    a += b; // a = a + b;
+    a /= 3; // a = a / 3;
+    a *= b; // a = a * b;
+
+    int score = 90;
+
+    //score 값을 1 증가 시키는 코드
+    ++score;
+    score++;
+    score = score + 1;
+    score += 1;
+
+    //삼항연산자
+    int q = 10 > 3 ? 5 : 1; // 조건식 ? (값 또는 연산식 : 값 또는 연산식);
+    System.out.println(q);
   }
 }
