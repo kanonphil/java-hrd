4163fdcdc269e420745bbb718d47e919e98b470f
kanonphil <xoghkdbwjd@gmail.com>
2025-12-17 16:51:14 +0900

    if

---
+++ Condition/src/If_3.java
@@ -0,0 +1,12 @@
+public class If_3 {
+  public static void main(String[] args) {
+    int num = 5;
+
+    if (num > 10) {
+      System.out.println(1);
+    }
+    else {
+      System.out.println(2);
+    }
+  }
+}
---
+++ Condition/src/If_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class If_4 {
+  public static void main(String[] args) {
+    //키보드로 정수를 하나 입력받아
+    //입력받은 정수가 양수면 1을 출력
+    //음수면 2를 출력, 0이면 3을 출력.
+    Scanner sc = new Scanner(System.in);
+    System.out.print("정수입력 : ");
+    int num = sc.nextInt();
+
+    if (num > 0) {
+      System.out.println(1);
+    } else if (num < 0) {
+      System.out.println(2);
+    } else if (num == 0) {
+      System.out.println(3);
+    }
+  }
+}
