a5d98a9ccd65759ac716cb15b8c03327862da9d2
kanonphil <xoghkdbwjd@gmail.com>
2025-12-18 12:19:35 +0900

    if / else-if / 실습문제

--- Condition/src/Test1_5.java
+++ Condition/src/Test1_5.java
@@ -4,17 +4,19 @@ public class Test1_5 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int num;
+    String grade = "C";
 
     System.out.print("0 ~ 100까지의(0, 100 포함) 숫자 중 임의의 값을 입력하세요 : ");
     num = sc.nextInt();
     if (num > 90 && num <= 100) {
-      System.out.println("학점은 A입니다.");
+      grade = "A";
     }
     else if (num > 80) {
-      System.out.println("학점은 B입니다.");
+      grade = "B";
     }
-    else {
-      System.out.println("학점은 C입니다.");
-    }
+    /*else {
+      grade = "C";
+    }*/
+    System.out.println("학점은 " + grade + "입니다.");
   }
 }
