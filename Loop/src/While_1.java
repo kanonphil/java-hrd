4dfbb6630840db32e39e0bc2874f7f9b6e6c7acf
kanonphil <xoghkdbwjd@gmail.com>
2025-12-19 10:20:49 +0900

    While

---
+++ Loop/src/While_1.java
@@ -0,0 +1,15 @@
+public class While_1 {
+  public static void main(String[] args) {
+    int num = 0;
+
+    System.out.println("시작");
+
+    while (num < 5){
+      System.out.println(1);
+      System.out.println(2);
+      num++;
+    }
+
+    System.out.println("종료");
+  }
+}
