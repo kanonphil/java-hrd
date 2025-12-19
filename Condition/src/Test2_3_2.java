9d2c5d86dfd76304577ef3bd2f78edd15bc618ba
kanonphil <xoghkdbwjd@gmail.com>
2025-12-19 09:32:44 +0900

    switch case break / Exercise

--- Condition/src/Test2_3_2.java
+++ Condition/src/Test2_3_2.java
@@ -5,24 +5,31 @@ public class Test2_3_2 {
     Scanner sc = new Scanner(System.in);
     System.out.print("1~99 사이의 정수를 입력하시오 >> ");
     int num = sc.nextInt();
-    int m = num % 10;
+    int tens = num / 10;
+    int ones = num % 10;
+    int clapCnt = 0;
 
-    switch (m) {
-      case 0:
-        int s = num / 10;
-        switch (s % 10){
-          case 3:
-            System.out.println("박수짝");
-            break;
-          case 6:
-            System.out.println("박수짝");
-            break;
-          case 9:
-            System.out.println("박수짝");
-            break;
-          default:
-            break;
-        }
+    /*if (tens == 3 || tens == 6 || tens == 9) {
+
+    }
+    if (ones == 3 || ones == 6 || ones == 9) {
+
+    }*/
+
+    if (tens % 3 == 0 && tens != 0) {
+      clapCnt++;
+    }
+
+    if (ones % 3 == 0 && ones != 0) {
+      clapCnt++;
+    }
+
+    switch (clapCnt) {
+      case 1:
+        System.out.println("박수짝");
+        break;
+      case 2:
+        System.out.println("박수짝짝");
     }
   }
 }
