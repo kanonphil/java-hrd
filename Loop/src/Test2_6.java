2fa302be499a9bfa76791dbb0995195b33d0a219
kanonphil <xoghkdbwjd@gmail.com>
2025-12-22 12:44:02 +0900

    break continue Exercise

--- Loop/src/Test2_6.java
+++ Loop/src/Test2_6.java
@@ -13,5 +13,19 @@ public class Test2_6 {
         System.out.println(i);
       }
     }
+
+    System.out.println("====================");
+
+    for (int i = 2; i < 31; i++) {
+      int cnt = 0;
+      for (int j = 1; j < i + 1; j++) {
+        if (i % j == 0) {
+          cnt++;
+        }
+      }
+      if (cnt == 2) {
+        System.out.println(i);
+      }
+    }
   }
 }
