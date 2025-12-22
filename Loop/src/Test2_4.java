be5ee52ff29dc95360e4b69e021a2adba3bfbd1b
kanonphil <xoghkdbwjd@gmail.com>
2025-12-22 12:02:22 +0900

    break continue Exercise

--- Loop/src/Test2_4.java
+++ Loop/src/Test2_4.java
@@ -1,11 +1,9 @@
 public class Test2_4 {
   public static void main(String[] args) {
-    int sum = 0;
-//    int lastNum;
     int i = 1;
+    int sum = 0;
     while (true) {
       sum += i;
-//      lastNum = i;
       if (sum > 50){
         break;
       }
