ef8a8b5b551d76e5d20e4eb7f94fd66635dfb601
kanonphil <xoghkdbwjd@gmail.com>
2025-12-22 12:13:55 +0900

    break continue Exercise

--- Loop/src/Test2_2.java
+++ Loop/src/Test2_2.java
@@ -1,10 +1,12 @@
 public class Test2_2 {
   public static void main(String[] args) {
-    for (int i = 1; i < 10; i++){
+    int i = 1;
+    while (true) {
       System.out.println(i);
-      if (i == 7){
+      if (i == 7) {
         break;
       }
+      i++;
     }
   }
 }
