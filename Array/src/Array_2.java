7e3a7c0b47e8276f6437d1ac95287d707fd47c1d
kanonphil <xoghkdbwjd@gmail.com>
2025-12-22 15:36:55 +0900

    Array

--- Array/src/Array_2.java
+++ Array/src/Array_2.java
@@ -11,5 +11,24 @@ public class Array_2 {
 
     //배열을 출력할 때는 명확히 몇 번째 요소를 출력할지 코드로 작성해야 함.
     System.out.println(arr1[0]);
+
+    //정수를 10개 저장할 수 있는 배열 arr2를 선언 및 생성
+    int[] arr2 = new int[10];
+
+    //arr2 배열의 모든 요소의 값을 출력하세요.
+    for (int i = 0; i < arr2.length; i++) {
+      System.out.println(arr2[i]);
+    }
+
+    System.out.println();
+
+    //arr2 배열의 0번째 요소에 5를 저장
+    arr2[0] = 5;
+
+    //arr2 배열의 모든 요소에 3을 저장
+    for (int i = 0; i < arr2.length; i++) {
+      arr2[i] = 3;
+      System.out.println("arr2[" + i + "] = " + arr2[i]);
+    }
   }
 }
