4bc62b76d151f56dbd9ad60ee13c9b72da40b98a
kanonphil <xoghkdbwjd@gmail.com>
2025-12-22 15:19:00 +0900

    Array

---
+++ Array/src/Array_2.java
@@ -0,0 +1,15 @@
+public class Array_2 {
+  public static void main(String[] args) {
+    //배열 값 읽고 쓰기
+
+    //실수를 3개 저장할 수 있는 배열 arr1을 선언 및 생성
+    double[] arr1 = new double[3];
+
+    //arr1 배열에 저장된 데이터를 출력
+    //배열 변수를 출력하면 이상한 데이터가 나온다.
+    System.out.println(arr1);
+
+    //배열을 출력할 때는 명확히 몇 번째 요소를 출력할지 코드로 작성해야 함.
+    System.out.println(arr1[0]);
+  }
+}
