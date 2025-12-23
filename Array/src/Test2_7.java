7135b38ebb6e2fdc1f844b20c5ded83556501131
kanonphil <xoghkdbwjd@gmail.com>
2025-12-23 14:21:54 +0900

    Array

--- Array/src/Test2_7.java
+++ Array/src/Test2_7.java
@@ -4,7 +4,7 @@ public class Test2_7 {
   public static void main(String[] args) {
     int[] arr = new int[10];
     for (int i = 0; i < arr.length; i++) {
-      arr[i] = (int)(Math.random() * 100) + 1;
+      arr[i] = (int)(Math.random() * 100 + 1);
     }
 
     System.out.println(Arrays.toString(arr));
