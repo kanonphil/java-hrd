57013fb2024d18fe3afcffe32ecc3884558a788e
kanonphil <xoghkdbwjd@gmail.com>
2025-12-23 16:42:14 +0900

    Method

--- Method/src/Method_1.java
+++ Method/src/Method_1.java
@@ -2,6 +2,11 @@ public class Method_1 {
   public static void main(String[] args) {
     // 메서드(method)는 모든 프로그래밍 언어에 존재하는 문법
     // 함수 - 특징 기능을 정의한 것
-    //
+    hello();
+  }
+
+  // 메서드의 정의
+  public static void hello() {
+    System.out.println("ㅎㅇ");
   }
 }
