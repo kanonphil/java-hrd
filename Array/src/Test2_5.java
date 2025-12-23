4ce382b760ce1022fa177f55731e9a173d08644e
kanonphil <xoghkdbwjd@gmail.com>
2025-12-23 12:20:31 +0900

    Array

--- Array/src/Test2_5.java
+++ Array/src/Test2_5.java
@@ -1,13 +1,17 @@
 // 시험 문제
 
+import java.util.Arrays;
+
 public class Test2_5 {
   public static void main(String[] args) {
     int[] arr = new int[6];
     for (int i = 0; i < arr.length; i++) {
       arr[i] = (int)(Math.random() * 45) + 1;
     }
-    for (int i = 0; i < arr.length; i++) {
+
+    System.out.println(Arrays.toString(arr));
+    /*for (int i = 0; i < arr.length; i++) {
       System.out.println(arr[i]);
-    }
+    }*/
   }
 }
