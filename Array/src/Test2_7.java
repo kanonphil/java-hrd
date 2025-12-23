119c7a2644127b3ce44cf7b0842cf65f70ca7591
kanonphil <xoghkdbwjd@gmail.com>
2025-12-23 14:11:49 +0900

    Array

--- Array/src/Test2_7.java
+++ Array/src/Test2_7.java
@@ -5,7 +5,7 @@ public class Test2_7 {
       arr[i] = (int)(Math.random() * 100) + 1;
     }
 
-    int max = 0;
+    int max = arr[0];
     int min = arr[0];
     for (int i = 0; i < arr.length; i++) {
       System.out.println(arr[i]);
