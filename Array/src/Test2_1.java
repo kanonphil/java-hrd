c049dd950bcf829d208434aa7c353bb75cb2ee8c
kanonphil <xoghkdbwjd@gmail.com>
2025-12-23 09:46:11 +0900

    Array

--- Array/src/Test2_1.java
+++ Array/src/Test2_1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Test2_1 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.print("배열의 길이를 입력: ");
     int n = sc.nextInt();
     int[] arr = new int[n];
     int sum = 0;
