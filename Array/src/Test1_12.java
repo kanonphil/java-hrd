4680852872414cd947dc85b8fed207b40d7203ae
kanonphil <xoghkdbwjd@gmail.com>
2025-12-23 15:12:39 +0900

    Array

--- Array/src/Test1_12.java
+++ Array/src/Test1_12.java
@@ -3,14 +3,27 @@ import java.util.Scanner;
 public class Test1_12 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int[] arr = new int[3];
-    int total = 0;
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] = sc.nextInt();
-      total += arr[i];
+    int[] scores = new int[4];
+    String[] sbj = {"국어", "영어", "수학"};
+    double avg;
+
+    for (int i = 0; i < scores.length - 1; i++) {
+      while (true) {
+        System.out.print(sbj[i] + " 점수(0 ~ 100) : ");
+        scores[i] = sc.nextInt();
+
+        if (scores[i] >= 0 && scores[i] <= 100) {
+          break;
+        }
+      }
     }
-    int average = total / 3;
-    System.out.println("총 점: " + total);
-    System.out.println("평균: " + average);
+
+    for (int i = 0; i < scores.length - 1; i++) {
+      System.out.println(sbj[i] + "점수 = " + scores[i]);
+      scores[scores.length - 1] += scores[i];
+    }
+    avg = (double)scores[scores.length - 1] / sbj.length;
+    System.out.println("총점 = " + scores[scores.length - 1]);
+    System.out.println("평균 = " + avg);
   }
 }
