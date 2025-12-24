ad4a4947d44f21d929d25549ded3d0677fb077e3
kanonphil <xoghkdbwjd@gmail.com>
2025-12-24 14:20:02 +0900

    Method Test

--- Method/src/Test1_10.java
+++ Method/src/Test1_10.java
@@ -9,11 +9,12 @@ public class Test1_10 {
   }
 
   public static void checkEvenOdd(int a) {
-    if (a % 2 == 0) {
+    System.out.println(a % 2 == 0 ? "짝수입니다" : "홀수입니다");
+    /*if (a % 2 == 0) {
       System.out.println("짝수입니다");
     }
     else {
       System.out.println("홀수입니다");
-    }
+    }*/
   }
 }
