ff69f52d8530a1499ce644d5cc2077e2c35e5c1b
kanonphil <xoghkdbwjd@gmail.com>
2025-12-24 14:22:40 +0900

    Method Test

--- Method/src/Test2_8.java
+++ Method/src/Test2_8.java
@@ -7,7 +7,7 @@ public class Test2_8 {
   }
 
   public static double getAvg(int... numbers) {
-    //.orElse(0,0)은 값이 없으면 0.0을 대신 반환
+    //.orElse(0.0)은 값이 없으면 0.0을 대신 반환
     return Arrays.stream(numbers).average().orElse(0.0);
   }
 
