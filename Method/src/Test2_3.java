0201f0f2c1af11b0250f19c691b1ee2a6c6b94c9
kanonphil <xoghkdbwjd@gmail.com>
2025-12-24 15:53:56 +0900

    Method Test

--- Method/src/Test2_3.java
+++ Method/src/Test2_3.java
@@ -1,10 +1,10 @@
 public class Test2_3 {
   public static void main(String[] args) {
-    int result = getBigNum(10, 20);
+    int result = getMax(10, 20);
     System.out.println(result);
   }
 
-  public static int getBigNum(int num1, int num2) {
+  public static int getMax(int num1, int num2) {
     return Math.max(num1, num2);
   }
 }
--- Method/src/Test2_4.java
+++ Method/src/Test2_4.java
@@ -1,10 +1,10 @@
 public class Test2_4 {
   public static void main(String[] args) {
-    String result = getString("안녕", "하세요");
+    String result = sumString("안녕", "하세요");
     System.out.println(result);
   }
 
-  public static String getString(String str1, String str2) {
+  public static String sumString(String str1, String str2) {
     return str1 + str2;
   }
 }
