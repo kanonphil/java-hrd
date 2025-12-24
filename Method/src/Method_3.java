4eb3a617049e9744b93a511aec6165e134031a94
kanonphil <xoghkdbwjd@gmail.com>
2025-12-24 10:30:52 +0900

    Method

--- Method/src/Method_3.java
+++ Method/src/Method_3.java
@@ -1,12 +1,14 @@
 public class Method_3 {
   public static void main(String[] args) {
     System.out.println("시작");
-    printAge();
+    printAge(20);
+    printAge(21);
+    printAge(22);
     System.out.println("종료");
   }
 
   //나이를 출력하는 메서드 정의
-  public static void printAge() {
-
+  public static void printAge(int age) {
+    System.out.println("나이는 " + age);
   }
 }
