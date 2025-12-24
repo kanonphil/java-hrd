6804f0221d61f141fc0317d9453608f36fb822c0
kanonphil <xoghkdbwjd@gmail.com>
2025-12-24 10:32:26 +0900

    Method Test

---
+++ Method/src/Test2_10.java
@@ -0,0 +1,14 @@
+public class Test2_10 {
+  public static void main(String[] args) {
+    System.out.println(isEven(10));
+  }
+
+  public static boolean isEven(int num) {
+    if (num % 2 == 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
