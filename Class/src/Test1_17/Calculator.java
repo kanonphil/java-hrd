e3b543b625cde1f767d5630f00241ef8fd06de18
kanonphil <xoghkdbwjd@gmail.com>
2025-12-26 17:24:43 +0900

    Class

---
+++ Class/src/Test1_17/Calculator.java
@@ -0,0 +1,40 @@
+package Test1_17;
+
+public class Calculator {
+  int num1;
+  int num2;
+  int result;
+  String operator;
+
+  public void setNum1(int num1) {
+    this.num1 = num1;
+  }
+
+  public void setNum2(int num2) {
+    this.num2 = num2;
+  }
+
+  public void setOperator(String operator) {
+    this.operator = operator;
+  }
+
+  public void calc() {
+    switch (this.operator) {
+      case "+":
+        result = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + result);
+        break;
+      case "-":
+        result = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + result);
+        break;
+      case "*":
+        result = num1 * num2;
+        System.out.println(num1 + " * " + num2 + " = " + result);
+        break;
+      case "/":
+        result = num1 / num2;
+        System.out.println(num1 + " / " + num2 + " = " + result);
+    }
+  }
+}
