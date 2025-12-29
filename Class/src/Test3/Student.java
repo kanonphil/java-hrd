fe7ae7de72a2a16adf4f3ab132b238618eb7761b
kanonphil <xoghkdbwjd@gmail.com>
2025-12-29 16:04:02 +0900

    Class

--- Class/src/Test3/Student.java
+++ Class/src/Test3/Student.java
@@ -1,16 +1,22 @@
 package Test3;
 
 public class Student {
+  private String id;
   private String name;
   private String age;
   private String phoneNumber;
   private String grade;
 
-  public Student(String name, String age, String phoneNumber, String grade) {
+  public Student(String id, String name, String age, String phoneNumber, String grade) {
+    this.id = id;
     this.name = name;
     this.age = age;
     this.phoneNumber = phoneNumber;
     this.grade = grade;
+  }
+
+  public String getId() {
+    return id;
   }
 
   public String getName() {
@@ -21,12 +27,12 @@ public class Student {
     return age;
   }
 
-  public String getGrade() {
-    return grade;
-  }
-
   public String getPhoneNumber() {
     return phoneNumber;
+  }
+
+  public String getGrade() {
+    return grade;
   }
 
   public void setPhoneNumber(String phoneNumber) {
--- Class/src/Test3/StudentManager.java
+++ Class/src/Test3/StudentManager.java
@@ -21,6 +21,17 @@ public class StudentManager {
     }
 
     System.out.println("학생 등록을 시작합니다. 학생 정보를 입력하세요.");
+    System.out.print("학번 : ");
+    String id = sc.nextLine();
+
+    // 학번 중복 확인
+    for (int i = 0; i < studentCount; i++) {
+      if (students[i].getId().equals(id)) {
+        System.out.println("이미 등록된 학생입니다.");
+        return;
+      }
+    }
+
     System.out.print("이름 : ");
     String name = sc.nextLine();
     System.out.print("나이 : ");
@@ -30,17 +41,7 @@ public class StudentManager {
     System.out.print("학점 : ");
     String grade = sc.nextLine();
 
-    // 이름 중복 확인
-    for (int i = 0; i < studentCount; i++) {
-      // 덮어쓰기
-      if (students[i].getName().equals(name)) {
-        students[i] = new Student(name, age, phoneNumber, grade);
-        System.out.println();
-        return;
-      }
-    }
-
-    Student student = new Student(name, age, phoneNumber, grade);
+    Student student = new Student(id, name, age, phoneNumber, grade);
     students[studentCount++] = student;
     System.out.println();
   }
@@ -48,13 +49,14 @@ public class StudentManager {
   // 학생정보변경(연락처)
   public void changePhoneNumber() {
     System.out.println("학생의 연락처를 변경합니다.");
-    System.out.print("변경 학생 : ");
-    String name = sc.nextLine();
+    System.out.print("변경 학생 학번 : ");
+    String id = sc.nextLine();
+//    String name = sc.nextLine();
 
-    // 입력된 이름의 학생 찾기
+    // 입력된 학번의 학생 찾기
     Student targetStudent = null;
     for (int i = 0; i < studentCount; i++) {
-      if (students[i].getName().equals(name)) {
+      if (students[i].getId().equals(id)) {
         targetStudent = students[i];
         break;
       }
@@ -77,12 +79,13 @@ public class StudentManager {
 
   // 학생 정보 출력
   public void printStudent() {
-    System.out.print("정보를 열람할 학생 : ");
-    String name = sc.nextLine();
+    System.out.print("정보를 열람할 학생의 학번 : ");
+    String id = sc.nextLine();
+//    String name = sc.nextLine();
 
     Student targetStudent = null;
     for (int i = 0; i < studentCount; i++) {
-      if (students[i].getName().equals(name)) {
+      if (students[i].getId().equals(id)) {
         targetStudent = students[i];
         break;
       }
@@ -94,7 +97,8 @@ public class StudentManager {
     }
 
     System.out.println("요청하신 학생의 정보입니다.");
-    System.out.print("이름 : " + targetStudent.getName());
+    System.out.print("학번 : " + targetStudent.getId());
+    System.out.print(", 이름 : " + targetStudent.getName());
     System.out.print(", 나이 : " + targetStudent.getAge());
     System.out.print(", 연락처 : " + targetStudent.getPhoneNumber());
     System.out.println(", 학점 : " + targetStudent.getGrade());
@@ -106,7 +110,8 @@ public class StudentManager {
     System.out.println("모든 학생의 정보입니다. 현재 총 학생 수는 " + studentCount + "명입니다.");
     for (int i = 0; i < studentCount; i++) {
       Student s = students[i];
-      System.out.print("이름 : " + s.getName());
+      System.out.print("학번 : " + s.getId());
+      System.out.print(", 이름 : " + s.getName());
       System.out.print(", 나이 : " + s.getAge());
       System.out.println(", 학점 : " + s.getGrade());
     }
@@ -133,6 +138,7 @@ public class StudentManager {
           break;
         case "5":
           System.out.println("프로그램을 종료합니다.");
+          sc.close();
           return;
         default:
           System.out.println("잘못된 입력입니다.");
