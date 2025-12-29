9d4bf78fe727090756615ffce3a66f33a2b294b6
kanonphil <xoghkdbwjd@gmail.com>
2025-12-29 10:41:20 +0900

    Class

---
+++ Class/src/Test2_1/StudentManager.java
@@ -0,0 +1,10 @@
+package Test2_1;
+
+public class StudentManager {
+  public static void main(String[] args) {
+    Student s = new Student();
+    s.setInfo("김자바", 20, "울산");
+    s.setSungjuk(80, 70, 90);
+    s.displayStudentInfo();
+  }
+}
