f5e059015d4645c360127014e138d5629261a3bd
kanonphil <xoghkdbwjd@gmail.com>
2025-12-30 16:12:46 +0900

    Class

--- Class/src/stu/Main.java
+++ Class/src/stu/Main.java
@@ -3,6 +3,7 @@ package stu;
 public class Main {
   public static void main(String[] args) {
     StudentService s = new StudentService();
+    System.out.println("학생 관리 프로그램을 실행합니다.");
     s.run();
   }
 }
