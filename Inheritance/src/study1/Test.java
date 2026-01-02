7e68caa2d4afee55c17a360d84da0245adf1762e
kanonphil <xoghkdbwjd@gmail.com>
2026-01-02 16:38:20 +0900

    Inheritance

--- Inheritance/src/study1/Test.java
+++ Inheritance/src/study1/Test.java
@@ -2,6 +2,10 @@ package study1;
 
 public class Test {
   public static void main(String[] args) {
+    Junior j = new Junior();
+    j.devel();
 
+    Senior s = new Senior();
+    s.devel();
   }
 }
