4819d3cfaf3501d48697891b27c065f421669a65
kanonphil <xoghkdbwjd@gmail.com>
2026-01-05 14:50:28 +0900

    Object 클래스

--- Inheritance/src/study3/Student.java
+++ Inheritance/src/study3/Student.java
@@ -3,12 +3,18 @@ package study3;
 //클래스 선언 시 어떠한 클래스도 상속받지 않으면
 //자동으로 Object 클래스를 상속한다.
 
+
 // 결론 : 자바의 모든 클래스는 Object 클래스를 상속한다.
 // 파생 결론 : Object 클래스로 모든 객체를 받을 수 있다.
 public class Student {
   private int stuNum;
   private String name;
   private int score;
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
 
   //매개변수에 무엇이든 다 넣겠다..
   public void aaa(Object a) {
--- Inheritance/src/study3/StudentTest.java
+++ Inheritance/src/study3/StudentTest.java
@@ -14,6 +14,8 @@ public class StudentTest {
     //현재 객체와 매개변수로 전달된 데이터가 같으면 true를 리턴하는 메서드
     //같다라는 의미는 개발자가 정하는 거다.
     //그렇지만 자바에서 의미하는 기본적인 같다의 의미는 '같은 주소값을 참조하고 있다'이다.
+    //equals 메서드를 원래 기능대로 쓰지말고, 개발자가 입맛대로 같다는 의미를
+    //재정의해서 사용하세요
 
     UnivFriend f1 = new UnivFriend("aa", "010", "aa");
     UnivFriend f2 = new UnivFriend("aa", "010", "aa");
