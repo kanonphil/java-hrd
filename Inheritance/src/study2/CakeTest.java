30fc4fe6c07c785bd45743e66defc5d29e0584eb
kanonphil <xoghkdbwjd@gmail.com>
2026-01-05 10:31:35 +0900

    Inheritance

---
+++ Inheritance/src/study2/CakeTest.java
@@ -0,0 +1,12 @@
+package study2;
+
+public class CakeTest {
+  public static void main(String[] args) {
+    CheeseCake c1 = new StrawberryCheeseCake();
+
+    //c1객체는 eatStrawberryCheeseCake() 메서드 호출은 불가!
+
+    //형변환을 통해 객체를 변경할 수 있다. int a = (int)10.5;
+    StrawberryCheeseCake c2 = c1;
+  }
+}
