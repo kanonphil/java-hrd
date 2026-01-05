6d75a118561a6d582f2683a1c5db0e268ebfa96a
kanonphil <xoghkdbwjd@gmail.com>
2026-01-05 12:37:49 +0900

    상속, 다형성, 오버라이딩

--- Inheritance/src/study2/FriendManager.java
+++ Inheritance/src/study2/FriendManager.java
@@ -5,11 +5,11 @@ public class FriendManager {
     Friend[] fs = new Friend[10];
     int cnt = 0;
 
-    fs[cnt++] = new UnivFriend("LEE", "Computer", "010-333-555");
-    fs[cnt++] = new UnivFriend("SEO", "Electronics", "010-222-444");
+    fs[cnt++] = new UnivFriend("LEE", "010-333-555", "Computer");
+    fs[cnt++] = new UnivFriend("SEO", "010-222-444", "Electronics");
 
-    fs[cnt++] = new CompFriend("YOON", "R&D 1", "02-123-999");
-    fs[cnt++] = new CompFriend("PARK", "R&D 2", "02-321-777");
+    fs[cnt++] = new CompFriend("YOON", "02-123-999", "R&D 1");
+    fs[cnt++] = new CompFriend("PARK", "02-321-777", "R&D 2");
 
     for (int i = 0; i < cnt; i++) {
       fs[i].showInfo();
