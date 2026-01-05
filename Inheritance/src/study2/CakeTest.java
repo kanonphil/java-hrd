c989efd6802333c7481c22255a398ea13347f8ce
kanonphil <xoghkdbwjd@gmail.com>
2026-01-05 11:06:16 +0900

    Inheritance

--- Inheritance/src/study2/CakeTest.java
+++ Inheritance/src/study2/CakeTest.java
@@ -7,6 +7,12 @@ public class CakeTest {
     //c1객체는 eatStrawberryCheeseCake() 메서드 호출은 불가!
 
     //형변환을 통해 객체를 변경할 수 있다. int a = (int)10.5;
-    StrawberryCheeseCake c2 = c1;
+    StrawberryCheeseCake c2 = (StrawberryCheeseCake)c1;
+    c2.eatStrawberryCheeseCake();
+
+    CheeseCake c3 = new CheeseCake();
+    StrawberryCheeseCake c4 = (StrawberryCheeseCake)c3;
+
+
   }
 }
---
+++ Inheritance/src/study2/Dog.java
@@ -0,0 +1,4 @@
+package study2;
+
+public class Dog {
+}
