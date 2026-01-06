3dad0dd07669c2e710f45f5282afd9aa6a8ef932
kanonphil <xoghkdbwjd@gmail.com>
2026-01-06 15:49:58 +0900

    Collection

---
+++ Collection/src/test1_1/Test1_1.java
@@ -0,0 +1,14 @@
+package test1_1;
+
+import java.util.ArrayList;
+
+public class Test1_1 {
+  public static void main(String[] args) {
+    ArrayList<String> str = new ArrayList<>();
+    str.add("a");
+    str.add("b");
+    str.add("c");
+
+    System.out.println(str);
+  }
+}
---
+++ Collection/src/test1_9/Student.java
@@ -0,0 +1,8 @@
+package test1_9;
+
+public class Student {
+  private String name;
+  private int korScore;
+  private int engScore;
+  private int 
+}
