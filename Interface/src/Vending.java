12b7d2f87f9f298621edd2e8a10b3a1c76e08a02
kanonphil <xoghkdbwjd@gmail.com>
2026-01-06 09:32:22 +0900

    Interface

---
+++ Interface/src/Vending.java
@@ -0,0 +1,14 @@
+//interface 안에는 메서드의 정의만 들어올 수 있음
+//중요한 것은 메서드의 내용부가 없다는 것이다. -> 추상메서드
+//interface 안에는 맴버변수 사용 못함, 생성자도 없음
+//interface는 생성자가 없기 때문에 객체 생성 x
+public interface Vending {
+  //커피 뽑는 기능
+  public void getCoffee();
+
+  //우유 뽑는 기능
+  public void getMilk();
+
+  //돈 넣기 기능
+  public void pay();
+}
