8e3833e0e4d295dd93427325936210755205fd41
kanonphil <xoghkdbwjd@gmail.com>
2026-01-06 10:32:30 +0900

    Interface

--- Interface/src/Machine.java
+++ Interface/src/Machine.java
@@ -1,7 +1,17 @@
 //자판기
 //implements : 구현
-//직역 : Vending interface를 구현해서 Machine class를 만들겠다
+//직역 : Vending interface의 추상메서드를 구현해서 Machine class를 만들겠다
 public class Machine implements Vending {
+  private int price;
+
+  public Machine() {
+
+  }
+
+  public void aaa() {
+
+  }
+
   @Override
   public void getCoffee() {
 
