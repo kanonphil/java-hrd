e7fec7ffbf4495dba46bdca0036276915e499148
kanonphil <xoghkdbwjd@gmail.com>
2026-02-03 09:59:09 +0900

    0203_test

--- test/src/test/Test5.java
+++ test/src/test/Test5.java
@@ -6,27 +6,21 @@ import java.util.Scanner;
 
 public class Test5 {
   public static void main(String[] args) {
-    // 난수 생성
-    int[] targetNumbers = generateRandomNumbers();
-
-    // 게임 루프
+    int[] targetNumbers = gameRandomNumbers();
     int attempts = 1;
 
-    System.out.println("만들어진 숫자 : " + Arrays.toString(targetNumbers));
+    //System.out.println("만들어진 숫자 : " + Arrays.toString(targetNumbers));
     System.out.println("숫자를 정했습니다. 게임을 시작합니다.");
+
     while (true) {
-      // 사용자 입력 받기
       System.out.print(attempts + " >> ");
       int[] userNumbers = getUserInput();
 
-      // 결과 체크
       int strikes = countStrikes(targetNumbers, userNumbers);
       int balls = countBalls(targetNumbers, userNumbers);
 
-      // 결과 표시
       System.out.println(strikes + " 스트라이크, " + balls + " 볼");
 
-      // 게임 종료 조건 검사
       if (strikes == 3) {
         System.out.println(attempts + "회만에 정답을 맞췄습니다.");
         break;
@@ -36,7 +30,7 @@ public class Test5 {
     }
   }
 
-  private static int[] generateRandomNumbers() {
+  private static int[] gameRandomNumbers() {
     int[] numbers = new int[3];
     Random random = new Random();
     for (int i = 0; i < 3; i++) {
@@ -59,36 +53,34 @@ public class Test5 {
   }
 
   private static int[] getUserInput() {
-    Scanner scanner = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     int[] numbers = new int[3];
     for (int i = 0; i < 3; i++) {
       while (true) {
         int number;
         try {
-          number = scanner.nextInt();
+          number = sc.nextInt();
         } catch (Exception e) {
           System.out.println("잘못된 입력입니다. 숫자를 다시 입력해 주세요.");
-          scanner.next(); // 잘못된 입력 스트림 비우기
-          continue; // 다시 입력 받음
+          sc.next();
+          continue;
         }
 
-        // 유효하지 않은 입력 처리
         if (number < 1 || number > 9) {
           System.out.println("잘못된 숫자입니다. 1부터 9까지의 숫자를 입력해 주세요.");
-          continue; // 다시 입력 받음
+          continue;
         }
-        // 중복 숫자 입력 처리
         boolean isUnique = true;
         for (int j = 0; j < i; j++) {
           if (numbers[j] == number) {
             System.out.println("중복된 숫자입니다. 다른 숫자를 입력해 주세요.");
             isUnique = false;
-            break; // 중복 발견 시 다시 입력
+            break;
           }
         }
         if (isUnique) {
           numbers[i] = number;
-          break; // 유효한 숫자면 반복 중단
+          break;
         }
       }
     }
